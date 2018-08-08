7207c593432d5faf9dc2f4d16292b0d4ac2bfa4d
Kevin <stradtkt22@gmail.com>
2018-08-07 23:36:53 -0400

    Added more media queries

--- assets/includes/about/about-block.php
+++ assets/includes/about/about-block.php
@@ -2,13 +2,13 @@
     <div class="container">
         <div class="row">
             <div class="col-sm-12 col-md-8 col-lg-8 offset-2 text-center">
-                <h1 class="text-primary">About CCRS</h1>
+                <h1 class="text-primary mt-5 mb-5">About CCRS</h1>
                 <p>
                 CCRS has many years of experience in evaluating credit and guiding consumers to assert their legal rights. 
                 We do it every day! We guarantee honesty and dependability, virtues which most people seem to have forgotten. Credit repair firms cannot do anything that you couldn't do yourself, 
                 but we can help you to achieve results in a fraction of the time without making costly errors. 
                 </p>
-                <p>
+                <p class="mb-5">
                 The software that we use speeds up the process of repairing your credit.  It would only take a matter of minutes every week to send in 
                 the items that you want to dispute.  The software is 100% error free and will only work to help you other than making your credit worse.
                 </p>
@@ -23,7 +23,6 @@
             <div class="col-sm-6 col-md-6 col-lg-6">
                 <div class="about-fix-credit">
                     <img src="assets/img/logo_new_2.png" alt="">
-                    <h1 class="text-center"></h1>
                         <h3 class="text-center">What Can CCRS do for you?</h3>
                         <p class="text-center">
                         We will be with you every step of the way.
